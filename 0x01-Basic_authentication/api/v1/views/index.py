9f0b7dcbb3060808123c3a979cc51b2028f4a1dc
ael-amin <alielamine6@gmail.com>
2023-11-09 07:34:50 -0800

    auth1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,18 +5,18 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """ GET /api/v1/unauthorizedi
     Return:
       - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route('/unauthorized/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """ GET /api/v1/unauthorized
     Return:
       - the number of each objects
     """
